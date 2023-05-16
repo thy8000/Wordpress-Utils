71d597228a67438f3e409d55ef4170bc99ddb1cc
Thunay <thunaymoreiradesoares@gmail.com>
2023-05-16 05:10:03 -0300

    Adiciona function para debug

--- wordpress-utils/functions/utils/_index.php
+++ wordpress-utils/functions/utils/_index.php
@@ -4,3 +4,4 @@ if (!defined('ABSPATH')) exit;
 
 include(plugin_dir_path(__FILE__) . 'custom-templates.php');
 include(plugin_dir_path(__FILE__) . 'include-function-path.php');
+include(plugin_dir_path(__FILE__) . 'debug.php');
---
+++ wordpress-utils/functions/utils/debug.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!defined('ABSPATH')) exit;
+
+function wp_utils_debug($log)
+{
+    $backtrace = debug_backtrace();
+
+    $caller = $backtrace[0]['file'] . ":" . $backtrace[0]['line'];
+
+    error_log('File: ' . $caller);
+    error_log(print_r($log, true));
+
+    return;
+}
