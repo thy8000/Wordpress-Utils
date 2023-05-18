67357dd9f8c991c7186b9fdc54709cd2cecb2128
Thunay <thunaymoreiradesoares@gmail.com>
2023-05-18 02:33:30 -0300

    Adiciona function de get-component

--- wordpress-utils/functions/utils/_index.php
+++ wordpress-utils/functions/utils/_index.php
@@ -4,4 +4,5 @@ if (!defined('ABSPATH')) exit;
 
 include(plugin_dir_path(__FILE__) . 'custom-templates.php');
 include(plugin_dir_path(__FILE__) . 'include-function-path.php');
+include(plugin_dir_path(__FILE__) . 'get-component.php');
 include(plugin_dir_path(__FILE__) . 'debug.php');
