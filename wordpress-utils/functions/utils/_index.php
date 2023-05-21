ca4383582acb0fe14a64ac4a108592d799546150
Thunay <thunaymoreiradesoares@gmail.com>
2023-05-21 16:56:43 -0300

    adiciona function load-svg e include-class

--- wordpress-utils/functions/utils/_index.php
+++ wordpress-utils/functions/utils/_index.php
@@ -6,3 +6,5 @@ include(plugin_dir_path(__FILE__) . 'custom-templates.php');
 include(plugin_dir_path(__FILE__) . 'include-function-path.php');
 include(plugin_dir_path(__FILE__) . 'get-component.php');
 include(plugin_dir_path(__FILE__) . 'debug.php');
+include(plugin_dir_path(__FILE__) . 'load-svg.php');
+include(plugin_dir_path(__FILE__) . 'include-class.php');
